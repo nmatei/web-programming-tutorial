8c4f81f0cab9baa5a569c62ec342188d78541410
Matei Nicolae <matei.nick@gmail.com>
2015-03-02 23:38:50 +0200

    move all js code from html in functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,28 @@
-var newsWidget = document.getElementById('news-widget');
-newsWidget.getElementsByClassName('header')[0].onclick = function(){
-    if(newsWidget.className == 'widget') {
-        newsWidget.className = 'widget collapsed';
+// show/hide more info
+document.getElementById('show-info').onclick = function () {
+    document.getElementById('more-info').style.display='inline';
+    document.getElementById('show-info').style.display='none';
+};
+
+document.getElementById('hide-info').onclick = function () {
+    document.getElementById('more-info').style.display='none';
+    document.getElementById('show-info').style.display='inline';
+};
+
+// collapse/expand news widget
+var newsWidget = document.getElementById("news-widget");
+newsWidget.getElementsByClassName("header")[0].onclick = function () {
+    if (newsWidget.className == "widget") {
+        newsWidget.className = "widget collapsed";
     } else {
-        newsWidget.className = 'widget';
+        newsWidget.className = "widget";
     }
 };
 
-
-var links = document.getElementById("top-menu").getElementsByTagName('a');
+// actions for top links
+var links = document.getElementById("top-menu").getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
-    links[i].onclick = function() {
-        document.getElementById('breadcrumb').innerHTML = this.innerHTML + ' : ' + this.title;
+    links[i].onclick = function () {
+        document.getElementById("breadcrumb").innerHTML = this.innerHTML + " : " + this.title;
     }
 }
