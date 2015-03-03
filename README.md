c65b96cfa797de496683e9522f2772747ed4c568
Matei Nicolae <matei.nick@gmail.com>
2015-03-03 23:15:25 +0200

    documentation for (Learning Pure JS)

--- README.md
+++ README.md
@@ -14,3 +14,20 @@ Exercises
     - and nested combinations of those
 - play in FireBug to find and change some attributes (color, display, width, height, etc.) of some elements
 - Make all widgets collapsible
+
+
+Learning Pure JS
+----------------
+
+- variables
+    - boolean | var y = true;
+    - number  | var i = 10;
+    - string  | var s = 'text';
+    - object
+        - array | var a = [1, 2, 3];
+        - json  | var p = {name: 'me', company: 'fasttrackit', age: 29, married: true, skills: ['html', 'css', 'js']};
+        - object (DOM elements, others)
+- functions
+    - parameters
+    - scope
+
---
+++ js/functions-jquery.js
@@ -0,0 +1,38 @@
+// show/hide more info
+$('#show-info').click(function () {
+    $('#more-info').show();
+    $('#show-info').hide();
+});
+
+$('#hide-info').click(function () {
+    $('#more-info').hide();
+    $('#show-info').show();
+});
+
+// collapse/expand news widget
+$(".widget .header").click(function () {
+    var widget = $(this).parent();
+    if (widget.hasClass('collapsed')) {
+        widget.removeClass('collapsed');
+    } else {
+        widget.addClass('collapsed');
+    }
+});
+
+// actions for top links
+$("#top-menu a").click(function () {
+    $("#breadcrumb").text(this.innerHTML + " : " + this.title);
+});
+
+
+// close widget with animation
+//$(".widget .header").click(function () {
+//    var widget = $(this).parent();
+//    widget.find('.content').animate({
+//        height: "toggle",
+//        opacity: "toggle"
+//    }, {
+//        duration: 400
+//    });
+//    widget.toggleClass('x-collapsed');
+//});
