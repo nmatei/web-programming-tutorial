f452b353a28e6da180e5b790c9ee8fe148e0bc55
Matei Nicolae <matei.nick@gmail.com>
2015-03-16 22:51:08 +0200

    add more ajax code examples (load one address, load list with addresses, load html template)

--- js/code-examples/ajax.js
+++ js/code-examples/ajax.js
@@ -1,11 +1,60 @@
+// = = = = = = = = = = = = = = = = = = = =
+// 1) AJAX example that load JSON from SERVER SIDE
+// = = = = = = = = = = = = = = = = = = = =
+
 $.ajax({
-    url: 'js/mocks/load-addresses.json',
+    url: 'js/mocks/load-address.json',
     method: 'POST',
     data: {
-        search: 'fasttrackit'
+        name: 'Daniel'
     }
 }).done(function (response) {
+    console.debug('address have been loaded:', response);
+    // in this case response is directly entire contact
+    console.info(response.name + ' - ' + response.phone);
+});
+
+
+// = = = = = = = = = = = = = = = = = = = =
+// 2) AJAX example that load a more COMPLEX JSON from SERVER SIDE
+// that contains a array with all contacts
+// = = = = = = = = = = = = = = = = = = = =
+
+$.ajax({
+    url: 'js/mocks/load-addresses.json',
+    method: 'POST'
+}).done(function (response) {
+    console.debug('addresses have been loaded:', response);
     if(response.success){
-        console.debug(response.data);
+        for (var i = 0 ; i < response.data.length ; i++) {
+            console.info(response.data[i].name + ' - ' + response.data[i].phone);
+        }
     }
 });
+
+// = = = = = = = = = = = = = = = = = = = =
+// 3) AJAX example that loads HTML content from SERVER SIDE
+// and add it at the end of element with id content (append)
+// = = = = = = = = = = = = = = = = = = = =
+
+function getServerCard() {
+    $.ajax({
+        url: 'templates/card.html',
+        method: 'GET',
+        data: {
+            name: 'A'
+        }
+    }).done(function (response) {
+        console.debug('card have been loaded');
+        console.debug(response);
+
+        // add card response at the end (concatenate) of 'content' element
+        document.getElementById('content').innerHTML += response;
+
+        // or more simple with jquery
+        //$("#content").append(response);
+    });
+}
+
+// call this function to load a card from server
+getServerCard();
