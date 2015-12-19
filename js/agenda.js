eddc9d0a1a1bc854b506472638d2f2c7718a16ad
Matei Nicolae <matei.nick@gmail.com>
2015-12-19 14:24:52 +0200

    add loadContacts, deleteContact. connect with server

--- js/agenda.js
+++ js/agenda.js
@@ -1,30 +1,61 @@
 console.info('listing agenda');
 
-function getRow(lastName, firstName, email) {
+function getRow(lastName, firstName, email, id) {
     var row = '<tr>'+
         '<td>' + lastName + '</td>' +
         '<td>' + firstName + '</td>' +
         '<td>' + email + '</td>' +
-        '<td><a href="#">Edit</a> <a href="#">Delete</a></td>' +
+        '<td>' +
+            '<a href="#" class="edit-contact" data-id="' + id + '">Edit</a> ' +
+            '<a href="#" class="delete-contact" data-id="' + id +  '">Delete</a>'+
+        '</td>' +
         '</tr>';
     return row;
 }
 
-var contacts = [
-    {lastName:"Popescu",     firstName:"Nicolae",  email:"Nicolae@example.com"},
-    {lastName:"Petrescu",    firstName:"Gabi",     email:"Gabi@example.com"},
-    {lastName:"Vasilescu",   firstName:"Raul",     email:"Raul@example.com"},
-    {lastName:"Ionescu",     firstName:"Denisa",   email:"Denisa@example.com"}
-];
+function showContacts(contacts) {
+    var agendaTable = document.getElementById("agenda");
+    var agendaBody = agendaTable.getElementsByTagName("tbody")[0];
+    var contactsHTML = '';
 
-var agendaTable = document.getElementById("agenda");
-var agendaBody = agendaTable.getElementsByTagName("tbody")[0];
+    for(var i = 0; i < contacts.length; i++) {
+        var person = contacts[i];
+        contactsHTML += getRow(person.lastName, person.firstName, person.email, person.id);
+    }
 
-var contactsHTML = '';
-
-for(var i = 0; i < contacts.length; i++) {
-    var person = contacts[i];
-    contactsHTML += getRow(person.lastName, person.firstName, person.email);
+    agendaBody.innerHTML = contactsHTML;
 }
 
-agendaBody.innerHTML = contactsHTML;
+function deleteContact(id) {
+    $.ajax({
+        url: "js/mocks/mark-done.json", // remove
+        cache: false,
+        method: 'POST',
+        data: {
+            id: id
+        }
+    }).done(function(status) {
+        console.debug(status);
+        loadContacts();
+    });
+}
+
+function loadContacts() {
+    $.ajax({
+        url: "js/mocks/load-agenda.json",
+        cache: false
+    }).done(function(contacts) {
+        console.debug('loaded contacts', contacts);
+        showContacts(contacts);
+
+        //document.getElementsByClassName("delete-contact")[0].click = function(){};
+        // vs jquery click
+        $('.delete-contact').click(function(){
+            var id = $(this).data('id');
+            console.debug('clicked on delete', id, this);
+            deleteContact(id);
+        });
+    });
+}
+
+loadContacts();
