1b78f06ff0fe3e00b6805fba7099054ef474e19a
Matei Nicolae <matei.nick@gmail.com>
2017-01-21 13:26:08 +0200

    add loadContacts & saveContact

--- js/contacte.js
+++ js/contacte.js
@@ -17,11 +17,27 @@ function showContacts(contacts) {
     $('#agenda tbody').html(contactsHTML);
 }
 
-$.ajax('servlets/contacts.json', {
-    success: function(contacts){
-        console.info('contacts loaded', contacts);
-        showContacts(contacts);
-    }
-});
+function loadContacts() {
+    $.ajax('servlets/contacts.json', {
+        success: function(contacts){
+            console.info('contacts loaded', contacts);
+            showContacts(contacts);
+        }
+    });
+}
+loadContacts();
+
+function saveContact(contact) {
+    $.ajax('servlets/saveContact.json', {
+        method: 'POST',
+        data: contact
+    });
+}
+
+// var contact = {
+//     nume: 'xxx',
+//     pren: 'eee'
+// };
+// saveContact(contact);
 
 console.info('page loaded');
