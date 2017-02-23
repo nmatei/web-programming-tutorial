63dea9addc2f527fdd037cd8ded69714ed849400
Matei Nicolae <matei.nick@gmail.com>
2017-02-23 20:47:40 +0200

    move add product into js

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,1 +1,11 @@
 console.info("shopping list init");
+
+function getRow(produs, cantitate) {
+    return '<tr>'+
+        '<td>' + produs + '</td>'+
+        '<td>' + cantitate + '</td>'+
+        '</tr>';
+}
+
+document.getElementsByTagName('tbody')[0].innerHTML = getRow("Paine", 1) + getRow("Suc", 3);
+
