396aaeb34404e061e14d61c0c166f4053c710718
Matei Nicolae <matei.nick@gmail.com>
2017-02-23 21:10:38 +0200

    add showList method

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -7,17 +7,20 @@ function getRow(produs, cantitate) {
         '</tr>';
 }
 
+function showList(shoppingList) {
+    var lista = '';
+    for(var i = 0; i < shoppingList.length; i++) {
+        var produs = shoppingList[i];
+        lista += getRow(produs.nume, produs.cantitate);
+    }
+
+    document.getElementsByTagName('tbody')[0].innerHTML = lista;
+}
+
 var shoppingList = [
     {nume: 'Paine', cantitate: 1},
     {nume: 'Suc', cantitate: 3},
     {nume: 'Mere', cantitate: 10}
 ];
 
-var lista = '';
-for(var i = 0; i < shoppingList.length; i++) {
-    var produs = shoppingList[i];
-    lista += getRow(produs.nume, produs.cantitate);
-}
-
-document.getElementsByTagName('tbody')[0].innerHTML = lista;
-
+showList(shoppingList);
