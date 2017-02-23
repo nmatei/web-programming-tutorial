9070e1cc886ccbe2304b678f2cc443a382d4cc29
Matei Nicolae <matei.nick@gmail.com>
2017-02-23 21:06:40 +0200

    iterate in shopping list and show all products

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -7,5 +7,17 @@ function getRow(produs, cantitate) {
         '</tr>';
 }
 
-document.getElementsByTagName('tbody')[0].innerHTML = getRow("Paine", 1) + getRow("Suc", 3);
+var shoppingList = [
+    {nume: 'Paine', cantitate: 1},
+    {nume: 'Suc', cantitate: 3},
+    {nume: 'Mere', cantitate: 10}
+];
+
+var lista = '';
+for(var i = 0; i < shoppingList.length; i++) {
+    var produs = shoppingList[i];
+    lista += getRow(produs.nume, produs.cantitate);
+}
+
+document.getElementsByTagName('tbody')[0].innerHTML = lista;
 
