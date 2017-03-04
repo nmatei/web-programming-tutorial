2623c35102603984ed1e8c336e62927c50783d22
Matei Nicolae <matei.nick@gmail.com>
2017-03-04 11:18:24 +0200

    create printTablaInmultii

--- intro.php
+++ intro.php
@@ -22,9 +22,17 @@ echo "<br>Nume Complet: " . getName($firstName, $name);
 
 <?php
 
-$n = 5;
-for($i = 1; $i <= 10; $i++) {
-    echo "<br>" . $i . " * " . $n . " = " . ($i * $n);
+function printTablaInmultii($n) {
+    echo "<br>----------";
+    for($i = 1; $i <= 10; $i++) {
+        $r = $i * $n;
+        echo "<br>$i * $n = $r";
+    }
 }
+
+for($i = 1; $i <= 10; $i++) {
+    printTablaInmultii($i);
+}
+
 
 ?>
