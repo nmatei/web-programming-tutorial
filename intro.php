c4f94d2a130d2ef07c82347be663e0310f5127fe
Matei Nicolae <matei.nick@gmail.com>
2017-03-04 11:06:32 +0200

    tabla inmultirii basic

---
+++ intro.php
@@ -0,0 +1,35 @@
+<?php
+echo "Suma este ";
+echo 4+3;
+
+$name = "matei";
+$a = 4;
+$b = 5;
+echo " Salut ";
+echo $name;
+$firstName = "Nicolae";
+echo "<br>" . $firstName . " " . $name;
+
+function getName($firstName, $name) {
+    return $firstName . " " . $name;
+}
+echo "<br>Nume Complet: " . getName($firstName, $name);
+
+?>
+
+<h1>Tabla inmultirii</h1>
+<br>
+
+<?php
+
+$n = 5;
+for($i = 1; $i <= 10; $i++) {
+    echo "<br>";
+    echo $i;
+    echo " * ";
+    echo $n;
+    echo " = ";
+    echo $i * $n;
+}
+
+?>
