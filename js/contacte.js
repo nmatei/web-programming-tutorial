1eca83c217f8e5c1d1786192dded10282a1fa5fc
Matei Nicolae <matei.nick@gmail.com>
2017-06-17 12:48:43 +0300

    load contacts from array of json objects

--- js/contacte.js
+++ js/contacte.js
@@ -1,22 +1,20 @@
 console.info('lista de contacte');
 
-//var contacts = ['Matei', 'Nicolae', 'Reut', 'Dan', 'Babut', 'Bogdan'];
 var contacts = [
-    ['Matei',   'Nicolae'],
-    ['Reut',    'Dan'],
-    ['Babut',   'Bogdan']
+    {phone: '0756000001', nume: 'Matei', prenume: 'Nicolae'},
+    {phone: '0756000002', nume: 'Reut', prenume: 'Dan'},
+    {phone: '0756000003', nume: 'Babut', prenume: 'Bogdan'}
 ];
 
 function getRow(nume, prenume, tel) {
-    var row = '<tr><td>' + nume + '</td><td>' + prenume + '</td><td>0756000001</td></tr>';
+    var row = '<tr><td>' + nume + '</td><td>' + prenume + '</td><td>'+ tel +'</td></tr>';
     return row;
 }
 
 var table = document.getElementById('contacts-list');
 var tbody = table.getElementsByTagName('tbody')[0];
 
-
 for(var i = 0; i < contacts.length; i++) {
     var contact = contacts[i];
-    tbody.innerHTML += getRow(contact[0], contact[1]);
+    tbody.innerHTML += getRow(contact.nume, contact.prenume, contact.phone);
 }
