6d3b85e65eac807b73dd7aa005b61e130eb66e73
Matei Nicolae <matei.nick@gmail.com>
2017-06-24 13:11:30 +0300

    add/remove contacts without refreshing page

--- js/contacte.js
+++ js/contacte.js
@@ -2,20 +2,14 @@ console.info('lista de contacte');
 
 function getRow(contact) {
     var row = '<tr><td>' + contact.nume + '</td><td>' + contact.prenume + '</td><td>'+ contact.phone +'</td>'+
-            '<td>[<a href="servlet/remove-contact.json?id=' + contact.id + '">x</a>]</td>'+
+            //'<td>[<a href="servlet/remove-contact.json?id=' + contact.id + '">x</a>]</td>'+
+            '<td>[<a href="#" class="remove-contact" data-id="'+ contact.id +'">x</a>]</td>'+
         '</tr>';
     return row;
 }
 
 var table = document.getElementById('contacts-list');
 var tbody = table.getElementsByTagName('tbody')[0];
-
-// for(var i = 0; i < contacts.length; i++) {
-//     var contact = contacts[i];
-//     tbody.innerHTML += getRow(contact.nume, contact.prenume, contact.phone);
-// }
-
-
 
 console.info("1) before load contacts");
 
@@ -25,6 +19,19 @@ $.ajax('servlet/contacte-list.json').done(function(contacts){
     contacts.forEach(function(contact) {
         tbody.innerHTML += getRow(contact);
     });
+
+    $('.remove-contact').click(function(){
+        var id = $(this).data('id');
+        console.info('remove this contact', id, this);
+        removeContact(id);
+        $(this).closest('tr').remove();
+    })
 });
 
 console.info("2) after load contacts");
+
+function removeContact(id) {
+    $.post('servlet/remove-contact.json', {
+        id: id
+    })
+}
