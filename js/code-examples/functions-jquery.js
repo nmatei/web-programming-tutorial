a5c9d7ee25d8f08ac03336ccec967f6a090721ef
Matei Nicolae <matei.nick@gmail.com>
2017-06-27 19:29:22 +0300

    fix widgets (close/hide)

--- js/code-examples/functions-jquery.js
+++ js/code-examples/functions-jquery.js
@@ -10,29 +10,26 @@ $('#hide-info').click(function () {
 });
 
 // collapse/expand all widgets
-$(".widget .header").click(function () {
-    var widget = $(this).parent();
-    if (widget.hasClass('collapsed')) {
-        widget.removeClass('collapsed');
-    } else {
-        widget.addClass('collapsed');
-    }
-});
+// $(".widget .header").click(function () {
+//     var widget = $(this).parent();
+//     widget.toggleClass('collapsed');
+// });
 
 // close widget with animation
-//$(".widget .header").click(function () {
-//    var widget = $(this).parent();
-//    widget.find('.content').animate({
-//        height: "toggle",
-//        opacity: "toggle"
-//    }, {
-//        duration: 400
-//    });
-//    widget.toggleClass('x-collapsed');
-//});
+$(".widget .header").click(function () {
+   var widget = $(this).parent();
+   widget.find('.content').animate({
+       height: "toggle",
+       opacity: "toggle"
+   }, {
+       duration: 400
+   });
+   widget.toggleClass('x-collapsed');
+});
 
 // actions for top links
-$("#top-menu a").click(function () {
-    $("#breadcrumb").text(this.innerHTML + " : " + this.title);
-});
+// $("#top-menu a").click(function () {
+//     $("#breadcrumb").text(this.innerHTML + " : " + this.title);
+//     return false;
+// });
 
